b3f9c6f1c46a32b7f3f282f7df61a5179d19055e
NotsoRay <157511314+NotsoRay@users.noreply.github.com>
2024-02-12 15:00:33 +0000

    emojis fix

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ const { GuildMember } = require("discord.js");
 
 module.exports = {
   emojis: {
-    true: '<:u_f:1200466558733004811>',
+    true: '<:u_t:1200466489703157831>',
     false: '<:u_f:1200466558733004811>'
   },
 
@@ -17,11 +17,11 @@ module.exports = {
    * @param {String} message
    * @param {GuildMember} member
    */
-  replaceAllMember: (message, member) => {
+  replaceAllMemberDescriptipn: (message, member) => {
     return message
     .replaceAll('{guild.name}', member.guild.name)
-    .replaceAll('{guild.icon}', member.guild.iconURL())
     .replaceAll('{guild.memberCount}', member.guild.memberCount)
+    .replaceAll('{member}', member)
     .replaceAll('{member::user.username}', member.user.username)
     .replaceAll('{member::user.avatar}', member.user.displayAvatarURL())
     .replaceAll('{member::user.id}', member.user.id)
