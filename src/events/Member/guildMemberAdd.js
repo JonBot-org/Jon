b4f27e8dac86ffb77ec089a3b8cedcaa6bd69c3e
Rayandevs <160603390+Rayandevs@users.noreply.github.com>
2024-02-21 18:50:31 +0000

    c

--- src/events/Member/guildMemberAdd.js
+++ src/events/Member/guildMemberAdd.js
@@ -9,72 +9,71 @@ module.exports.data = {
 
 /**
  * @param {import('discord.js').GuildMember} member
- * @param {import('discord.js').Client} client
  */
-module.exports.execute = async (member, client) => {
-  const { guild } = member;
-  const data = await guilds.findOne({ id: guild.id });
+module.exports.execute = async (member) => {
+  await member.fetch();
+  const data = await guilds.findOne({ id: member.guild.id });
 
-  if (data && data.configurations.greet.enabled) {
-    let messageObject = {};
-    if (data.configurations.greet.description) {
-      const embed = new EmbedBuilder()
-        .setDescription(
-          replaceVariables(
-            "d-m",
-            data.configurations.greet.description,
-            member,
-          ),
-        )
-        .setColor(
-          data.configurations.greet.color
-            ? Colors[data.configurations.greet.color]
-            : "Random",
-        );
+  if (!data) return;
 
-      if (data.configurations.greet.timestamp === "YES") {
-        embed.setTimestamp();
-      }
-
-      if (data.configurations.greet.author_icon) {
-        embed.setAuthor({
-          name: replaceVariables(
-            "a_n",
-            data.configurations.greet.author_name,
-            member,
-          ),
-          iconURL: replaceVariables(
-            "a_i",
-            data.configurations.greet.author_icon,
-            member,
-          ),
-        });
-      }
-
-      if (data.configurations.greet.title) {
-        embed.setTitle(
-          replaceVariables("t", data.configurations.greet.title, member),
-        );
-      }
-
-      messageObject = { embeds: [embed] };
-    }
-
-    if (data.configurations.greet.message) {
-      Object.assign(messageObject, {
-        content: replaceVariables(
+  if (data.configurations.greet.enabled) {
+    console.log(data.configurations.greet);
+    // Variables
+    const Options = {};
+    const message = data.configurations.greet.message
+      ? replaceVariables("d-m", data.configurations.greet.message, member)
+      : replaceVariables("d-m", "{user}, **Welcome!**", member);
+    const description = data.configurations.greet.description
+      ? replaceVariables("d-m", data.configurations.greet.description, member)
+      : replaceVariables(
           "d-m",
-          data.configurations.greet.message,
+          "✨ Created At: {user_createdTimestamp}&n&✨ Joined At: {user_joinedTimestamp}",
           member,
-        ),
-      });
+        );
+    const title = data.configurations.greet.title
+      ? replaceVariables("d-m", data.configurations.greet.title, member)
+      : replaceVariables("d-m", "{server_name} now has {server_members} members.", member);
+    const author_name = data.configurations.greet.author_name
+      ? replaceVariables("a-n", data.configurations.greet.author_name, member)
+      : replaceVariables("a-n", "{user_name}", member);
+    const author_icon = data.configurations.greet.author_icon
+      ? replaceVariables("a-i", data.configurations.greet.author_icon, member)
+      : replaceVariables("a-i", "{user_avatar}", member);
+    const color = data.configurations.greet.color
+      ? Colors[data.configurations.greet.color]
+      : Colors.DarkVividPink;
+    const timestamp = data.configurations.greet.timestamp;
+
+    console.log(
+      message,
+      description,
+      title,
+      author_name,
+      author_icon,
+      color,
+      timestamp,
+    );
+
+    const embed = new EmbedBuilder()
+      .setTitle(title)
+      .setDescription(description)
+      .setColor(color);
+
+    if (author_name !== undefined) {
+        embed.setAuthor({ name: author_name, iconURL: author_icon });
     }
 
-    const channel = await guild.channels.fetch(
+    if (timestamp === "YES") {
+      embed.setTimestamp();
+    }
+
+    Object.assign(Options, { content: message, embeds: [embed] });
+
+    const channel = await member.guild.channels.fetch(
       data.configurations.greet.channel,
     );
     if (channel) {
-      channel.send(messageObject);
+      channel.send(Options);
     }
   }
 };
